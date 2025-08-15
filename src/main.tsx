9df1856a33724bc4e705e64ff682c3b28bd2a804
hjb0304 <shany34@naver.com>
2025-08-15 23:56:45 +0900

    💄 style: 커스텀 변수 추가, 폰트 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './assets/custom.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
