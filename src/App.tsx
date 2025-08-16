6fecf09619dab4b2792df93ddc22141e25646397
hjb0304 <shany34@naver.com>
2025-08-16 23:11:49 +0900

    ✨ feat: 기본 레이아웃 구조 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import AppRoutes from './routes';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col gap-6 p-4 pt-0">
+        <AppRoutes />
+      </main>
+      <Navigation />
+    </>
+  );
 }
 
 export default App;
