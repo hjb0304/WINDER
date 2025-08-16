e5714d348c213ce3a2ab6b451b2df75ca4f19ecf
hjb0304 <shany34@naver.com>
2025-08-16 23:13:16 +0900

    ✨ feat: Navigation 컴포넌트 UI 구현

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,34 @@
+import { BookOpenText, Heart, House, List, Utensils } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  const navItems = [
+    { idx: 1, to: '/', label: '홈', icon: <House /> },
+    { idx: 2, to: '/records', label: '나의 와인 기록', icon: <List /> },
+    { idx: 3, to: '/favorite', label: '찜 목록', icon: <Heart /> },
+    { idx: 4, to: '/winelist', label: '전체 와인 목록', icon: <BookOpenText /> },
+    { idx: 5, to: '/pairing', label: '음식 페어링', icon: <Utensils /> },
+  ];
+
+  return (
+    <nav className="fixed bottom-0 w-full bg-white border-t border-t-lightgray">
+      <ul className="flex h-14">
+        {navItems.map((item) => (
+          <li className="w-full" key={item.idx}>
+            <NavLink
+              className={({ isActive }) =>
+                `w-full h-full justify-items-center content-center inline-block ${isActive ? 'text-primary' : 'text-subtext'}`
+              }
+              to={item.to}
+              title={item.label}
+            >
+              {item.icon}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
