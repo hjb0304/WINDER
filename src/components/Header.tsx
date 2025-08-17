5cbf882751333d71074a2cd087977d09192d6e95
hjb0304 <shany34@naver.com>
2025-08-17 23:51:14 +0900

    ✨ feat: Header에 링크 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import { ChevronLeft, User } from 'lucide-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className="flex justify-between items-center h-[72px] px-4 sticky bg-white">
-      <button>
+      <button onClick={() => navigate(-1)}>
         <ChevronLeft />
       </button>
       <h1 className="title">Header</h1>
-      <button>
+      <Link to="/my">
         <User />
-      </button>
+      </Link>
     </header>
   );
 }
