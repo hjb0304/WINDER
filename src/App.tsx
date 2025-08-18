a96e0245800d1d8d3fe22bd9452efcc512a9bbfd
hjb0304 <shany34@naver.com>
2025-08-18 17:28:10 +0900

    ✨ feat: 회원가입 페이지 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import Navigation from '@/components/Navigation';
 import Header from '@/components/Header';
 import AppRoutes from '@/routes';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
-    <>
-      {<Header />}
-      <main className="flex flex-col gap-6 p-4 pt-0">
+    <div
+      className={`min-h-screen ${pathname === '/login' || pathname === '/signup' ? 'bg-[#F9F5F1]' : 'bg-white'}`}
+    >
+      {pathname !== '/login' && pathname !== '/signup' && <Header />}
+      <main
+        className={`flex flex-col p-4 pt-0 ${pathname === '/login' || pathname === '/signup' ? 'gap-8' : 'gap-6'}`}
+      >
         <AppRoutes />
       </main>
-      <Navigation />
-    </>
+      {pathname !== '/login' && pathname !== '/signup' && <Navigation />}
+    </div>
   );
 }
 
--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,5 +1,59 @@
+import logo from '@/assets/img/logo.svg';
+import Button from '@/components/Button';
+import Checkbox from '@/components/Checkbox';
+import Input from '@/components/Input';
+import { CameraIcon } from 'lucide-react';
+
 function SignUpPage() {
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <div className="w-[220px] mx-auto mt-14">
+        <img src={logo} alt="WINDER 로고" />
+      </div>
+      <form>
+        <div className="space-y-3">
+          <div className="rounded-full bg-lightgray w-[100px] aspect-square flex justify-center items-center mx-auto">
+            <CameraIcon color="var(--color-gray)" fill="white" size={48} strokeWidth={1} />
+          </div>
+          <label htmlFor="email" className="inline-block mb-2 label">
+            이메일
+          </label>
+          <div className="flex gap-2">
+            <Input id="email" name="email" placeholder="이메일" />
+            <Button className="!h-11 shrink-0">중복 확인</Button>
+          </div>
+          <Input
+            id="password"
+            name="password"
+            label="비밀번호"
+            labelClassName="min-w-[82px] label"
+            placeholder="비밀번호"
+          />
+          <Input
+            id="passwordConfirm"
+            name="passwordConfirm"
+            label="비밀번호 확인"
+            labelClassName="min-w-[82px] label"
+            placeholder="비밀번호 확인"
+          />
+          <Input
+            id="nickname"
+            name="nickname"
+            label="닉네임"
+            labelClassName="min-w-[82px] label"
+            placeholder="닉네임"
+          />
+        </div>
+        <div className="my-4 space-y-3">
+          <Checkbox id="policy" name="policy" label="개인정보처리방침에 동의합니다." checked />
+          <Checkbox id="service" name="service" label="서비스 이용 약관에 동의합니다." />
+        </div>
+        <Button full submit>
+          회원가입
+        </Button>
+      </form>
+    </>
+  );
 }
 
 export default SignUpPage;
