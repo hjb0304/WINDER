75769d5fef1cbf9334f074056ea9f2ffe60261ea
hjb0304 <shany34@naver.com>
2025-08-19 15:30:54 +0900

    ✨ feat: Modal 컴포넌트 구현

---
+++ src/components/Modal.tsx
@@ -0,0 +1,66 @@
+import Button from '@/components/Button';
+import { X } from 'lucide-react';
+import { useEffect } from 'react';
+
+interface ModalProps {
+  isOpen: boolean;
+  message: string;
+  handleCancel: () => void;
+  handleConfirm: () => void;
+  hideCancelButton?: boolean;
+}
+
+function Modal({ isOpen, message, handleCancel, handleConfirm, hideCancelButton }: ModalProps) {
+  if (!isOpen) return null;
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isOpen]);
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center w-full h-full bg-black/70"
+      onClick={() => {
+        handleCancel();
+      }}
+    >
+      <div
+        className="relative flex flex-col items-center justify-center p-4 bg-white rounded-lg w-96 min-h-40"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="absolute cursor-pointer top-4 right-4"
+          type="button"
+          aria-label="닫기"
+          onClick={() => {
+            handleCancel();
+          }}
+        >
+          <X />
+        </button>
+        <p className="mb-8">{message}</p>
+        <div>
+          {!hideCancelButton && (
+            <Button
+              outlined
+              onClick={() => {
+                handleCancel();
+              }}
+            >
+              취소
+            </Button>
+          )}
+          <Button onClick={handleConfirm} size="sm">
+            확인
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
