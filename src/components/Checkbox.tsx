739436dc79bc5761309528a83495444afc2c5486
hjb0304 <shany34@naver.com>
2025-08-19 15:30:27 +0900

    ✨ feat: Checkbox 컴포넌트 onChange 속성 추가, 에러 메시지 추가

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,28 +5,44 @@ interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   id: string;
   name: string;
   label: string;
+  errorMessage?: string;
 }
 
-function Checkbox({ id, name, label, className, checked, ...props }: CheckboxProps) {
+function Checkbox({
+  id,
+  name,
+  label,
+  className,
+  checked,
+  errorMessage,
+  onChange,
+  ...props
+}: CheckboxProps) {
   return (
-    <div className="relative flex items-center gap-2">
-      <input
-        type="checkbox"
-        id={id}
-        name={name}
-        {...props}
-        className={`appearance-none outline-1 w-5 aspect-square rounded-sm bg-white ${checked ? 'outline-primary' : 'outline-lightgray'} ${className}`}
-      ></input>
-      {checked && (
-        <Check
-          color="var(--color-primary)"
-          size={16}
-          strokeWidth={3}
-          className="absolute left-0.5"
-        />
-      )}
-      <label htmlFor={id}>{label}</label>
-    </div>
+    <>
+      {' '}
+      <div className="relative flex items-center gap-2">
+        <input
+          type="checkbox"
+          id={id}
+          name={name}
+          {...props}
+          className={`appearance-none outline-1 w-5 aspect-square rounded-sm bg-white ${checked ? 'outline-primary' : 'outline-lightgray'} ${className}`}
+          checked={!!checked}
+          onChange={onChange}
+        ></input>
+        {checked && (
+          <Check
+            color="var(--color-primary)"
+            size={16}
+            strokeWidth={3}
+            className="absolute left-0.5"
+          />
+        )}
+        <label htmlFor={id}>{label}</label>
+      </div>
+      {<p className="text-xs text-error">{errorMessage}</p>}
+    </>
   );
 }
 
