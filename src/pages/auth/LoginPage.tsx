8026268cf4211803f67cd4ed54921b92edb1971b
hjb0304 <shany34@naver.com>
2025-08-19 15:32:10 +0900

    ✨ feat: 로그인 페이지 입력값 유효성 검증, 로그인 기능 추가

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -4,26 +4,75 @@ import logo from '@/assets/img/logo.svg';
 import Input from '@/components/Input';
 import kakaoLogo from '@/assets/img/kakao_logo.svg';
 import naverLogo from '@/assets/img/naver_logo.svg';
+import { useAuthStore } from '@/store/authStore';
+import { login } from '@/api/auth';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import type { UserInfo } from '@/type/user';
+import type { ApiError } from '@/type/api';
+import { useState } from 'react';
+import Modal from '@/components/Modal';
 
 function LoginPage() {
+  // 아이디/비밀번호 오류
+  const [error, setError] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const navigate = useNavigate();
+  const { setUser } = useAuthStore();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserInfo>();
+
+  // 로그인
+  const onSubmit: SubmitHandler<UserInfo> = async (data) => {
+    try {
+      const user = await login(data);
+      if (user) {
+        setUser(user);
+        navigate('/');
+      }
+    } catch (error) {
+      const e = error as ApiError;
+
+      if (e.code === 'user-not-found' || e.code === 'invalid-credential') {
+        setError('이메일 또는 비밀번호가 올바르지 않습니다.');
+      } else {
+        setError('로그인에 실패했습니다.');
+      }
+
+      setIsModalOpen(true);
+    }
+  };
 
   return (
     <>
       <div className="w-[220px] mx-auto mt-14">
         <img src={logo} alt="WINDER 로고" />
       </div>
-      <form>
-        <Input id="email" name="email" className="mb-2 mt-[1px]" placeholder="이메일" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="email"
+          className="mb-4 mt-[1px]"
+          placeholder="이메일"
+          errorMessage={errors.email?.message}
+          messageClassName="mt-[-16px] mb-2"
+          {...register('email', { required: '이메일을 입력해주세요.' })}
+        />
         <Input
           id="password"
-          name="password"
           label="비밀번호"
           className="mb-4"
           placeholder="비밀번호"
           labelClassName="sr-only"
+          errorMessage={errors.password?.message}
+          messageClassName="mt-[-16px] mb-2"
+          {...register('password', { required: '비밀번호를 입력해주세요.' })}
+          type="password"
         />
-        <Button full onClick={() => navigate('/')} submit>
+        <Button full submit>
           로그인
         </Button>
       </form>
@@ -46,6 +95,13 @@ function LoginPage() {
           </button>
         </div>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        message={error}
+        handleCancel={() => setIsModalOpen(false)}
+        handleConfirm={() => setIsModalOpen(false)}
+        hideCancelButton
+      ></Modal>
     </>
   );
 }
