a08085328f468f42c32a5ff68fe46ee505ac6e56
hjb0304 <shany34@naver.com>
2025-08-24 00:00:11 +0900

    ✨ feat: Select 컴포넌트 옵션 value props 추가

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,13 +2,13 @@ import { ChevronDown } from 'lucide-react';
 import type { SelectHTMLAttributes } from 'react';
 
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
-  options: string[];
+  options: { text: string; value: string }[];
   id: string;
   name: string;
   label: string;
 }
 
-function Select({ options, id, name, label, className }: SelectProps) {
+function Select({ options, id, name, label, className, onChange }: SelectProps) {
   return (
     <div className="relative">
       <label htmlFor={id} className="sr-only">
@@ -18,10 +18,11 @@ function Select({ options, id, name, label, className }: SelectProps) {
         id={id}
         name={name}
         className={`min-w-[100px] appearance-none px-4 rounded-lg outline-1 outline-lightgray h-10 bg-white ${className}`}
+        onChange={onChange}
       >
         {options.map((option, idx) => (
-          <option key={idx} value={option}>
-            {option}
+          <option key={idx} value={option.value}>
+            {option.text}
           </option>
         ))}
       </select>
