a2129e2a53771e65140ae2325a1aa43e1c9ca533
hjb0304 <shany34@naver.com>
2025-08-24 00:23:17 +0900

    ✨ feat: 와인 목록 필터링, 정렬, 검색 기능 추가 / id 형식 변경

--- src/pages/wineList/index.tsx
+++ src/pages/wineList/index.tsx
@@ -6,18 +6,29 @@ import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 
 function WineListPage() {
-  const [allData, setAllData] = useState<WineInfo[]>([]);
-  const [data, setData] = useState<WineInfo[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [loading, setLoading] = useState(false);
-
-  const wineOptions = ['전체', '레드', '화이트', '로제', '스파클링'];
-  const sortOptions = ['이름순', '별점순'];
-
+  const wineOptions = [
+    { text: '전체', value: 'all' },
+    { text: '레드', value: 'reds' },
+    { text: '화이트', value: 'whites' },
+    { text: '로제', value: 'rose' },
+    { text: '스파클링', value: 'sparkling' },
+  ];
+  const sortOptions = [
+    { text: '이름순', value: 'name' },
+    { text: '별점순', value: 'rating' },
+  ];
   const observer = useRef<IntersectionObserver | null>(null);
   const observerRef = useRef<HTMLDivElement | null>(null);
   const PER_PAGE = 10;
   const wineTypes = ['reds', 'whites', 'rose', 'sparkling'];
+
+  const [allData, setAllData] = useState<WineInfo[]>([]);
+  const [data, setData] = useState<WineInfo[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [loading, setLoading] = useState(false);
+  const [wineOption, setWineOption] = useState(wineOptions[0].value);
+  const [sortOption, setSortOption] = useState(sortOptions[0].value);
+  const [search, setSearch] = useState('');
 
   // 와인 데이터 불러오기
 
@@ -29,18 +40,19 @@ function WineListPage() {
           axios.get<APIWineInfo[]>(`https://api.sampleapis.com/wines/${type}`),
         ),
       );
-      const newData = res.flatMap((res) =>
+      const newData = res.flatMap((res, i) =>
         res.data.map(
           (data: APIWineInfo): WineInfo => ({
-            id: data.id,
+            id: `${wineTypes[i]}-${data.id}`,
             name: data.wine,
             country: data.location.split('\n')[0],
             imgURL: data.image,
             rating: data.rating.average,
+            type: wineTypes[i],
           }),
         ),
       );
-      setAllData(newData);
+      setAllData(newData.sort((a, b) => a.name.localeCompare(b.name)));
       // 첫 10개 데이터만 보여주기
       setData(newData.slice(0, PER_PAGE));
     } catch (error) {
@@ -76,19 +88,45 @@ function WineListPage() {
 
   // 다음 데이터 목록 불러오기
   useEffect(() => {
-    if (page === 1) return;
+    // 조건 설정 시 전체 데이터 변경
 
-    setData((prev) => [...prev, ...allData?.slice((page - 1) * PER_PAGE, page * PER_PAGE)]);
-  }, [page]);
+    // 필터링
+    let resultData =
+      wineOption !== 'all' ? allData.filter((item) => item.type === wineOption) : allData;
 
-  useEffect(() => {}, [page]);
+    // 정렬
+    resultData =
+      sortOption === 'rating'
+        ? [...resultData].sort((a, b) => Number(b.rating) - Number(a.rating))
+        : resultData;
+
+    // 검색
+    resultData =
+      search.trim() !== ''
+        ? resultData.filter((item) => item.name.toLowerCase().includes(search.toLowerCase().trim()))
+        : resultData;
+
+    setData(resultData.slice(0, page * PER_PAGE));
+  }, [page, wineOption, sortOption, search]);
 
   return (
     <div>
       <section>
         <div className="flex gap-2 mb-2">
-          <Select options={wineOptions} id="wine" name="wine" label="와인 종류" />
-          <Select options={sortOptions} id="sort" name="sort" label="정렬 기준" />
+          <Select
+            options={wineOptions}
+            id="wine"
+            name="wine"
+            label="와인 종류"
+            onChange={(e) => setWineOption(e.target.value)}
+          />
+          <Select
+            options={sortOptions}
+            id="sort"
+            name="sort"
+            label="정렬 기준"
+            onChange={(e) => setSortOption(e.target.value)}
+          />
         </div>
         <Input
           id="search"
@@ -96,6 +134,7 @@ function WineListPage() {
           placeholder="검색어를 입력해주세요."
           label="검색"
           labelClassName="sr-only"
+          onChange={(e) => setSearch(e.target.value)}
         />
       </section>
       {loading ? (
