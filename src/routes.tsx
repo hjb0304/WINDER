59730d6cc7fa1289d80c59c265510c044710ab14
hjb0304 <shany34@naver.com>
2025-08-24 20:36:20 +0900

    ✨ feat: 전체 route 설정

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import WineListPage from '@/pages/wineList';
 import RecordsPage from '@/pages/records';
 import LoginPage from '@/pages/auth/LoginPage';
 import SignUpPage from '@/pages/auth/SignUpPage';
+import WineDetail from '@/pages/wineList/Detail';
 
 function AppRoutes() {
   return (
@@ -15,10 +16,15 @@ function AppRoutes() {
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/signup" element={<SignUpPage />}></Route>
       <Route path="/my" element={<MyPage />}></Route>
+      <Route path="/my/info" element={<MyPage />}></Route>
       <Route path="/records" element={<RecordsPage />}></Route>
+      <Route path="/records/:id" element={<RecordsPage />}></Route>
+      <Route path="/records/new" element={<RecordsPage />}></Route>
       <Route path="/favorite" element={<FavoritePage />}></Route>
       <Route path="/winelist" element={<WineListPage />}></Route>
+      <Route path="/winelist/:id" element={<WineDetail />}></Route>
       <Route path="/pairing" element={<PairingPage />}></Route>
+      <Route path="/pairing/results" element={<PairingPage />}></Route>
     </Routes>
   );
 }
