be8137c2565ad1a130b07962bfed3edd183d6015
hjb0304 <shany34@naver.com>
2025-08-24 20:39:26 +0900

    ✨ feat: 찜하기 버튼 컴포넌트 구현

---
+++ src/components/FavoriteButton.tsx
@@ -0,0 +1,15 @@
+import { Heart } from 'lucide-react';
+
+function FavoriteButton({ onClick, isFavorite }: { onClick: () => void; isFavorite: boolean }) {
+  return (
+    <button
+      className={`absolute flex items-center justify-center w-10 rounded-full cursor-pointer aspect-square ${isFavorite ? 'bg-primary' : 'bg-white border-1 border-lightgray'} bottom-2 right-2`}
+      type="button"
+      onClick={onClick}
+    >
+      <Heart color="transparent" fill={isFavorite ? 'white' : 'var(--color-lightgray)'} />
+    </button>
+  );
+}
+
+export default FavoriteButton;
