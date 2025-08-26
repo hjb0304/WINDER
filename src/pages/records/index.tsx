78d4261fdecb8c9c636bf83483a7445a32d33ce7
hjb0304 <shany34@naver.com>
2025-08-26 19:16:14 +0900

    ✨ feat: 와인 기록 목록 데이터 조회 기능 추가, 필터링/정렬/검색 기능 추가

--- src/pages/records/index.tsx
+++ src/pages/records/index.tsx
@@ -1,7 +1,8 @@
+import { getWineList } from '@/api/wine';
 import Card from '@/components/Card';
 import Input from '@/components/Input';
 import Select from '@/components/Select';
-import type { WineInfo } from '@/type/wine';
+import type { MyWineInfo } from '@/type/wine';
 import { useEffect, useState } from 'react';
 
 function RecordsPage() {
@@ -13,11 +14,12 @@ function RecordsPage() {
     { text: '스파클링', value: 'sparkling' },
   ];
   const sortOptions = [
+    { text: '최신순', value: 'date' },
     { text: '이름순', value: 'name' },
     { text: '별점순', value: 'rating' },
   ];
 
-  const [data, setData] = useState<WineInfo[]>([]);
+  const [data, setData] = useState<MyWineInfo[]>([]);
   const [loading, setLoading] = useState(false);
   const [wineOption, setWineOption] = useState(wineOptions[0].value);
   const [sortOption, setSortOption] = useState(sortOptions[0].value);
@@ -27,8 +29,10 @@ function RecordsPage() {
   const getWineData = async () => {
     try {
       setLoading(true);
+      const data = await getWineList();
+      setData(data);
     } catch (error) {
-      console.error('데이터를 불러올 수 없습니다.');
+      console.error('데이터를 불러올 수 없습니다.', error);
     } finally {
       setLoading(false);
     }
@@ -37,6 +41,23 @@ function RecordsPage() {
   useEffect(() => {
     getWineData();
   }, []);
+
+  // 필터링
+  let resultData = wineOption !== 'all' ? data.filter((item) => item.type === wineOption) : data;
+
+  // 정렬
+  resultData =
+    sortOption === 'name'
+      ? [...resultData].sort((a, b) => a.name.localeCompare(b.name))
+      : sortOption === 'rating'
+        ? [...resultData].sort((a, b) => Number(b.rating) - Number(a.rating))
+        : resultData;
+
+  // 검색
+  resultData =
+    search.trim() !== ''
+      ? resultData.filter((item) => item.name.toLowerCase().includes(search.toLowerCase().trim()))
+      : resultData;
 
   return (
     <div>
@@ -70,16 +91,15 @@ function RecordsPage() {
         <p>데이터를 불러오는 중입니다.</p>
       ) : (
         <section>
-          {data?.map((item, i) => (
+          {resultData?.map((item) => (
             <Card
               key={item.id}
-              imgURL={item.imgURL}
+              imgURL={item.imgURL?.[0] ?? ''}
               name={item.name}
               rating={item.rating}
               url={`${item.id}`}
             ></Card>
           ))}
-          <div></div>
         </section>
       )}
     </div>
