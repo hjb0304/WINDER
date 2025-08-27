d1b808529ebe22693bb1b884230b2bd542ed2106
hjb0304 <shany34@naver.com>
2025-08-27 20:45:05 +0900

    ✨ feat: 메인 페이지 카드에 url 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
-import SubTitle from '@/components/subTitle';
 import logo from '@/assets/img/logo.svg';
 import Card from '@/components/Card';
+import SubTitle from '@/components/SubTitle';
 
 function HomePage() {
   return (
@@ -15,7 +15,7 @@ function HomePage() {
       </section>
       <section>
         <SubTitle>최근 기록</SubTitle>
-        <Card name="Catena Malbec" type="Malbec" date="2025-08-15" />
+        <Card name="Catena Malbec" type="Malbec" date="2025-08-15" imgURL="" url="" />
       </section>
     </>
   );
