d3a60050e90cd089ba0ca265ffd2073b2991bd8e
hjb0304 <shany34@naver.com>
2025-08-27 20:39:30 +0900

    ✨ feat: 찜 목록 등록 함수 id와 데이터 받도록 수정

--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -1,23 +1,24 @@
+import type { WineInfo } from './../type/wine';
 import { auth, db } from '@/lib/firebase';
 import { collection, deleteDoc, doc, getDocs, serverTimestamp, setDoc } from 'firebase/firestore';
 
 // 찜 목록에 와인 추가
-export async function addFavorite(wineId: string) {
+export async function addFavorite(data: WineInfo) {
   if (!auth.currentUser) throw new Error('로그인이 필요합니다.');
 
-  const favRef = doc(db, 'users', auth.currentUser.uid, 'favorites', wineId);
-  await setDoc(favRef, { createdAt: serverTimestamp() });
+  const favRef = doc(db, 'users', auth.currentUser.uid, 'favorites', data.id);
+  await setDoc(favRef, { ...data, createdAt: serverTimestamp() });
 }
 
 // 찜한 와인 목록 조회
-export async function getFavoriteList() {
+export async function getFavoriteList(): Promise<WineInfo[]> {
   if (!auth.currentUser) throw new Error('로그인이 필요합니다.');
 
   // id가 일치하는 사용자의 기록 목록만 가져오기
   const favCol = collection(db, 'users', auth.currentUser.uid, 'favorites');
   const snapshot = await getDocs(favCol);
 
-  return snapshot.docs.map((doc) => doc.id);
+  return snapshot.docs.map((doc) => ({ docId: doc.id, ...(doc.data() as WineInfo) }));
 }
 
 // 찜 목록에서 와인 삭제
