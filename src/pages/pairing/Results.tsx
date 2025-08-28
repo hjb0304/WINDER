026e1cd3a9e06649370d233f8aedec22909f00c9
hjb0304 <shany34@naver.com>
2025-08-28 15:59:27 +0900

    ✨ feat: 페어링 결과 페이지 params 값에 따른 결과 표시

--- src/pages/pairing/Results.tsx
+++ src/pages/pairing/Results.tsx
@@ -1,5 +1,78 @@
+import SubTitle from '@/components/SubTitle';
+import { pairingResultData } from '@/data/pairing';
+
+import { Smile, ThumbsUp, Trophy } from 'lucide-react';
+import { useParams } from 'react-router-dom';
+
 function PairingResultsPage() {
-  return <div></div>;
+  const { name } = useParams<{ name: keyof typeof pairingResultData }>();
+
+  return (
+    <>
+      <section className="mb-4 space-y-2">
+        <div className="rounded-lg overflow-hidden">
+          <img src={name && pairingResultData[name].imgURL} alt="" />
+        </div>
+        <SubTitle>{name ? pairingResultData[name].name : ''}</SubTitle>
+        <p>{name && pairingResultData[name].desc}</p>
+      </section>
+      <section>
+        <ul>
+          <li className="py-4 border-b border-lightgray flex gap-2">
+            <div className="space-y-1">
+              <p className="flex items-center gap-1.5">
+                <span className="flex justify-center items-center rounded-full w-8 h-8 bg-primary">
+                  <Trophy color="white" size={20} />
+                </span>
+                <span>최고의 페어링</span>
+              </p>
+              <span className="inline-block">
+                {name && pairingResultData[name].pairing[0].name}
+              </span>
+              <p className="text-subtext sub">{name && pairingResultData[name].pairing[0].desc}</p>
+            </div>
+            <div className="rounded-lg w-[6.25rem] aspect-square shrink-0 overflow-hidden">
+              <img src={name && pairingResultData[name].pairing[0].imgURL} alt="" />
+            </div>
+          </li>
+          <li className="py-4 border-b border-lightgray flex gap-2">
+            <div className="space-y-1">
+              <p className="flex items-center gap-1.5">
+                <span className="flex justify-center items-center rounded-full w-8 h-8 bg-[#C97B7B]">
+                  <ThumbsUp color="white" size={20} />
+                </span>
+                <span>잘 어울려요</span>
+              </p>
+              <span className="inline-block">
+                {name && pairingResultData[name].pairing[1].name}
+              </span>
+              <p className="text-subtext sub">{name && pairingResultData[name].pairing[0].desc}</p>
+            </div>
+            <div className="rounded-lg w-[6.25rem] aspect-square shrink-0 overflow-hidden">
+              <img src={name && pairingResultData[name].pairing[1].imgURL} alt="" />
+            </div>
+          </li>
+          <li className="py-4 border-b border-lightgray flex gap-2">
+            <div className="space-y-1">
+              <p className="flex items-center gap-1.5">
+                <span className="flex justify-center items-center rounded-full w-8 h-8 bg-[#E6CBA8]">
+                  <Smile color="white" size={20} />
+                </span>
+                <span>무난해요</span>
+              </p>
+              <span className="inline-block">
+                {name && pairingResultData[name].pairing[2].name}
+              </span>
+              <p className="text-subtext sub">{name && pairingResultData[name].pairing[0].desc}</p>
+            </div>
+            <div className="rounded-lg w-[6.25rem] aspect-square shrink-0 overflow-hidden">
+              <img src={name && pairingResultData[name].pairing[2].imgURL} alt="" />
+            </div>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
 }
 
 export default PairingResultsPage;
