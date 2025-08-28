8304a6acbdfe07fec7e889252c246f9ec4407ee3
hjb0304 <shany34@naver.com>
2025-08-28 15:55:45 +0900

    🍱 asset: 와인/음식 이미지 파일 추가

---
+++ src/assets/img/index.ts
@@ -0,0 +1,39 @@
+import cabernetSauvignon from '@/assets/img/cabernet_sauvignon.jpg';
+import cheddar from '@/assets/img/cheddar.jpg';
+import cheese from '@/assets/img/cheese.jpg';
+import chicken from '@/assets/img/chicken.jpg';
+import creamPasta from '@/assets/img/cream_pasta.jpg';
+import dessert from '@/assets/img/dessert.jpg';
+import fish from '@/assets/img/fish.jpg';
+import friedChicken from '@/assets/img/fried_chicken.jpg';
+import fruits from '@/assets/img/fruits.jpg';
+import ham from '@/assets/img/ham.jpg';
+import mushroom from '@/assets/img/mushroom.jpg';
+import oyster from '@/assets/img/oyster.jpg';
+import redmeat from '@/assets/img/redmeat.jpg';
+import salad from '@/assets/img/salad.jpg';
+import shrimp from '@/assets/img/shrimp.jpg';
+import sparkling from '@/assets/img/sparkling.jpg';
+import spicyAsia from '@/assets/img/spicy_asia.jpg';
+import spicyPork from '@/assets/img/spicy_pork.jpg';
+
+export const wineImg = {
+  cabernetSauvignon,
+  cheddar,
+  cheese,
+  chicken,
+  creamPasta,
+  dessert,
+  fish,
+  friedChicken,
+  fruits,
+  ham,
+  mushroom,
+  oyster,
+  redmeat,
+  salad,
+  shrimp,
+  sparkling,
+  spicyAsia,
+  spicyPork,
+};
