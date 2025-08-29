1f195add5de91e7edfbcf3d5f9070b29e36e43ce
hjb0304 <shany34@naver.com>
2025-08-29 23:57:25 +0900

    ✨ feat: 프로필 이미지 업로드 컴포넌트 기능 구현

--- src/components/ProfileImgUpload.tsx
+++ src/components/ProfileImgUpload.tsx
@@ -1,9 +1,37 @@
 import wine from '@/assets/img/icon/wine.svg';
+import { Camera } from 'lucide-react';
 
-function ProfileImgUpload() {
+function ProfileImgUpload({
+  url,
+  onChange,
+  canUpload = true,
+}: {
+  url: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  canUpload?: boolean;
+}) {
   return (
-    <div className="rounded-full bg-lightgray w-[100px] aspect-square flex justify-center items-center mx-auto">
-      <img src={wine} alt="와인" className="w-12 h-auto" />
+    <div className="rounded-full bg-lightgray w-[100px] aspect-square mx-auto relative">
+      <div className=" w-[100px] aspect-square overflow-hidden rounded-full flex justify-center items-center">
+        <img src={url ? url : wine} alt="와인" className={url ? '' : 'w-12 h-auto'} />
+      </div>
+      {canUpload && (
+        <>
+          <label
+            htmlFor="photoURL"
+            className="absolute right-0 flex items-center justify-center rounded-full w-9 bottom-1.5 aspect-square bg-gray cursor-pointer"
+          >
+            <Camera color="white" size={18} />
+          </label>
+          <input
+            type="file"
+            accept="image/*"
+            id="photoURL"
+            className="hidden"
+            onChange={onChange}
+          />
+        </>
+      )}
     </div>
   );
 }
