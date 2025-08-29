e0b300c67fac06c364f6dd83f79af5531f91d4f2
hjb0304 <shany34@naver.com>
2025-08-29 13:50:16 +0900

    ✨ feat: 로그아웃, 회원 탈퇴, 비밀번호 변경, 프로필 사진 변경 함수 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,6 +6,8 @@ import {
   updateProfile,
   signInWithEmailAndPassword,
   signOut,
+  deleteUser,
+  updatePassword,
 } from 'firebase/auth';
 
 // 회원가입
@@ -35,9 +37,49 @@ export async function login({ email, password }: UserInfo) {
 }
 
 // 로그아웃
-export async function logout({}) {
+export async function logout() {
   try {
     await signOut(auth);
+  } catch (error) {
+    const e = error as ApiError;
+    throw e;
+  }
+}
+
+// 회원 탈퇴
+export async function deleteAccount(user: typeof auth.currentUser) {
+  try {
+    if (user) await deleteUser(user);
+  } catch (error) {
+    const e = error as ApiError;
+    throw e;
+  }
+}
+
+// 프로필 이미지 변경
+export async function editProfileImg(user: typeof auth.currentUser, newImgURL: string) {
+  try {
+    if (user) await updateProfile(user, { photoURL: newImgURL });
+  } catch (error) {
+    const e = error as ApiError;
+    throw e;
+  }
+}
+
+// 비밀번호 변경
+export async function editPassword(user: typeof auth.currentUser, newPassword: string) {
+  try {
+    if (user) await updatePassword(user, newPassword);
+  } catch (error) {
+    const e = error as ApiError;
+    throw e;
+  }
+}
+
+// 닉네임 변경
+export async function editNickname(user: typeof auth.currentUser, newNickname: string) {
+  try {
+    if (user) await updateProfile(user, { displayName: newNickname });
   } catch (error) {
     const e = error as ApiError;
     throw e;
