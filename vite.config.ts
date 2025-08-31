a40a752d9c2caae8fb19816c350d1074aaa2177d
hjb0304 <shany34@naver.com>
2025-08-31 21:58:44 +0900

    ✨ feat: 이미지 최적화, webP 변환용 imagemin 패키지 설치

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminWebp from 'imagemin-webp';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp({ quality: 70 }),
+          png: imageminWebp({ quality: 70 }),
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
