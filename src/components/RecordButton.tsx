261d154f01b68d305f76f1f10c6869433670f85b
hjb0304 <shany34@naver.com>
2025-08-31 23:36:19 +0900

    ✨ feat: 와인 기록 버튼에 aria-label 추가

--- src/components/RecordButton.tsx
+++ src/components/RecordButton.tsx
@@ -9,6 +9,7 @@ function RecordButton() {
       type="submit"
       className="fixed flex items-center justify-center rounded-full bg-primary w-14 aspect-square right-4 bottom-[72px] cursor-pointer shadow-md"
       onClick={() => navigate('/records/new')}
+      aria-label="와인 기록하기"
     >
       <Plus color="white" />
     </button>
