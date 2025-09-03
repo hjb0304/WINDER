29d93985258fb594bc5886f1f0936b230526cf17
hjb0304 <shany34@naver.com>
2025-09-03 13:47:06 +0900

    ✨ feat: 기록하기 버튼 스크롤 방향에 따라 숨김/표시 기능 구현

--- src/components/RecordButton.tsx
+++ src/components/RecordButton.tsx
@@ -1,15 +1,43 @@
 import { Plus } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function RecordButton() {
   const navigate = useNavigate();
 
+  const [visible, setVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // 위로 스크롤 시 버튼 나타남
+      if (currentScrollY < lastScrollY) {
+        setVisible(true);
+      } else {
+        // 아래로 스크롤 시 버튼 숨김
+        setVisible(false);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <button
       type="submit"
-      className="fixed flex items-center justify-center rounded-full bg-primary w-14 aspect-square right-4 bottom-[72px] cursor-pointer shadow-md"
+      className={`fixed flex items-center justify-center rounded-full bg-primary w-14 aspect-square right-4 bottom-[72px] cursor-pointer shadow-md transition duration-300
+          ${!visible ? 'opacity-0' : 'opacity-100'}`}
       onClick={() => navigate('/records/new')}
       aria-label="와인 기록하기"
+      aria-hidden={!visible}
     >
       <Plus color="white" />
     </button>
